1844c4914a1be2b957980e326ed1741db538423c
JonasProg <j.vester@stud.uni-heidelberg.de>
2021-04-10 12:02:19 +0200

    new main

--- src/main.py
+++ src/main.py
@@ -3,7 +3,12 @@ import circumference as c
 
 x = input('Choose: square or circle')
 if x == 'square':
-    print('I like squares')
+    x = input('area or circumference?')
+    if x == 'area':
+        x = input('length of side?')
+        return a.area_square(x)
+if x == 'circle':
+
 else:
     print('Idiot')
 
