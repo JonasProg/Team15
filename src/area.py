d2f97bc30ba7a19da2539c1bd21f534fc194659e
Andreas Albers <albers0andreas@gmail.com>
2021-04-10 11:59:41 +0200

    add circle functions

--- src/area.py
+++ src/area.py
@@ -1,8 +1,19 @@
+from math import pi
+
 def area_circle(r):
+    '''Area of circle.
+
+    Calculate the area of a circle based on the radius.
+
+    Args:
+        r (float): radius of circle
+
+    Returns:
+        float: area of circle
     '''
-    asdgfudisgfudash
-    '''
-    return True
+    if r < 0:
+        raise ValueError("The radius must be >= 0.")
+    return pi*r**2
 
 def area_square(l):
     return True
--- src/circumference.py
+++ src/circumference.py
@@ -1,5 +1,19 @@
+from math import pi
+
 def circ_circle(r):
-    return
+    '''Circumference of circle.
+
+    Calculate the circumference of a circle based on the radius.
+
+    Args:
+        r (float): radius of circle
+
+    Returns:
+        float: circumference of circle
+    '''
+    if r < 0:
+        raise ValueError("The radius must be >= 0.")
+    return pi*r
 
 def circ_square(l):
     return
