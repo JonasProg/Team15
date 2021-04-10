96f7ccda270a4fce59ade99be1ec1065eb4146e0
JonasProg <j.vester@stud.uni-heidelberg.de>
2021-04-10 11:57:30 +0200

    Hey

--- src/area.py
+++ src/area.py
@@ -1,3 +1,4 @@
+import numpy as np
 def area_circle(r):
     '''
     asdgfudisgfudash
@@ -5,4 +6,17 @@ def area_circle(r):
     return True
 
 def area_square(l):
-    return True
+    """Calculates the area of a square.
+
+    Extended description of function.
+
+    Args:
+        l (float) : l is the length of side of a square.
+
+    Returns:
+        float: area of a square.
+    """
+    if l < 0:
+        raise ValueError("The length of side must be >= 0.")
+    area_output= l**2
+    return area_output
