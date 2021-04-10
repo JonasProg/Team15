5eeb0aa2811fd756d2c9277135b21bb5634d1763
JonasProg <j.vester@stud.uni-heidelberg.de>
2021-04-10 15:58:04 +0200

    wohoo

--- src/pytest/test_all.py
+++ src/pytest/test_all.py
@@ -2,25 +2,60 @@ import pytest
 import area as a
 import circumference as c
 from math import pi
+
 @pytest.mark.circle
 @pytest.mark.parametrize('myinput, myref',
                          [(1, pi),
                           (0, 0),
                           (2.1, pi * 2.1**2),
-                          (-5, pytest.raises(ValueError)),
                           ])
-def circle_area_tester(myinput,myref):
-    """Test values of circle area again reference values of r >= 0, and r < 0."""
+def test_circle_area_tester(myinput,myref):
+    """Test values of circle area against reference values of r >= 0, and r < 0."""
     print(myinput)
     assert a.area_circle(myinput) == myref
+def test_values1():
+    """Make sure value errors are recognized for area_circle."""
+    with pytest.raises(ValueError):
+        a.area_circle(-5)
 @pytest.mark.square
 @pytest.mark.parametrize('myinput, myref',
                          [(1, 1),
                           (0, 0),
                           (2.1, 2.1**2),
-                          (-5, pytest.raises(ValueError)),
                           ])
-def square_area_tester(myinput,myref):
-    """Test values of square area again reference values of r >= 0, and r < 0."""
+def test_square_area_tester(myinput,myref):
+    """Test values of square area against reference values of r >= 0, and r < 0."""
     print(myinput)
     assert a.area_square(myinput) == myref
+def test_values2():
+    """Make sure value errors are recognized for area_square."""
+    with pytest.raises(ValueError):
+        a.area_square(-5)
+@pytest.mark.circle
+@pytest.mark.parametrize('myinput, myref',
+                         [(1, 2 * pi),
+                          (0, 0),
+                          (2.1, 2 *pi * 2.1),
+                          ])
+def test_circle_circumference_tester(myinput,myref):
+    """Test values of circle circumference against reference values of r >= 0, and r < 0."""
+    print(myinput)
+    assert c.circ_circle(myinput) == myref
+def test_values3():
+    """Make sure value errors are recognized for area_circle."""
+    with pytest.raises(ValueError):
+        c.circ_circle(-5)
+@pytest.mark.square
+@pytest.mark.parametrize('myinput, myref',
+                         [(1, 1 * 4),
+                          (0, 0),
+                          (2.1, 2.1*4),
+                          ])
+def test_square_circumference_tester(myinput,myref):
+    """Test values of square circumference against reference values of r >= 0, and r < 0."""
+    print(myinput)
+    assert c.circ_square(myinput) == myref
+def test_values4():
+    """Make sure value errors are recognized for area_square."""
+    with pytest.raises(ValueError):
+        c.circ_square(-5)
