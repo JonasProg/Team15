4e21aa314ca1e6f26f0a7768d52ff7f30516832e
JonasProg <j.vester@stud.uni-heidelberg.de>
2021-04-10 12:18:27 +0200

    update

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 import area as a
 import circumference as c
 
-x = input('Choose: square or circle')
-if x == 'square':
-    x = input('area or circumference?')
-    if x == 'area':
-        x = input('length of side?')
-        return a.area_square(x)
-if x == 'circle':
-
-else:
-    print('Idiot')
-
+shape = input('Choose: square or circle: ')
+if shape == 'square':
+    y = input('area or circumference?: ')
+    if y == 'area':
+        length = input('enter length of side in cm: ')
+        output = a.area_square(float(length))
+        print('The area of your square is {:.2f} cm².'.format(output))
+    elif y == 'circumference':
+        length = input('enter length of side in cm: ')
+        output = c.circ_square(float(length))
+        print('The circumference of your square is {:.2f} cm.'.format(output))
+    else:
+        print('Please restart with correct syntax.')
